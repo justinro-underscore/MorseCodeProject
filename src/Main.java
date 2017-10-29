3e4cf5b196df9a088b8066ce580eb687c1dcc8d1
Justin Roderman <jcroderman@gmail.com>
2017-10-29 18:20:50 -0500

    Adds morse code output through speaker

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /*
  * Name: Justin Roderman
@@ -10,8 +20,19 @@ import java.util.Scanner;
  * Desc: Simple program to receive input as string and output as morse code
  */
 
+/*
+ * TODO:
+ * 		- Change from Clip to Midi for morse code output
+ * 		- Create input from microphone <- This will take a while
+ * 		- Don't hard code EVERYTHING
+ */
+
 public class Main
 {
+	// For morse code speaker
+	final static int SPACE_BETWEEN_WORDS = 300; // .3 seconds
+	final static int SPACE_BETWEEN_LETTERS = 200; // .2 seconds
+	
 	public static  HashMap<Character, String> morseDictionary; // Morse Code Dictionary
 	
 	public static void main(String[] args)
@@ -19,7 +40,9 @@ public class Main
 		programInitialize(); // Must run first
 		
 		String in = input();
-		System.out.println(convertToMorse(in));
+		String morse = convertToMorse(in);
+		System.out.println(morse);
+		playMorse(morse);
 	}
 	
 	// Populates the morseDictionary
@@ -73,5 +96,78 @@ public class Main
 				result += (morseDictionary.get(key) + ' ');
 		}
 		return result;
+	}
+	
+	// Converts the morse code to sound
+	public static void playMorse(String input)
+	{
+		Scanner chopper = new Scanner(input); // Go through the input
+		String letter; // Each morse code letter
+		while(chopper.hasNext())
+		{
+			letter = chopper.next();
+			if(letter == "|") // If it's a space, wait for a bit
+			{
+				try { Thread.sleep(SPACE_BETWEEN_WORDS); }
+				catch (InterruptedException e) { e.printStackTrace(); }
+			}
+			else // If a letter, go through the letter
+			{
+				for(int i = 0; i < letter.length(); i++)
+				{
+					char temp = letter.charAt(i);
+					// Determines whether the char is a short or long beep
+					if(temp == '.')
+						playSound(true);
+					else if(temp == '-')
+						playSound(false);
+				}
+			}
+			try { Thread.sleep(SPACE_BETWEEN_LETTERS); } // Pause for next letter
+			catch (InterruptedException e) { e.printStackTrace(); }
+		}
+		chopper.close();
+	}
+	
+	// Plays a note
+	// Code from https://www.youtube.com/watch?v=nUKya2DvYSo
+	public static void playSound(boolean shortLength)
+	{
+		Mixer.Info[] mixInfos = AudioSystem.getMixerInfo();
+		
+		Mixer mixer = AudioSystem.getMixer(mixInfos[0]);
+		
+		Clip clip = null;
+		DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
+		try { clip = (Clip) mixer.getLine(dataInfo); }
+		catch(LineUnavailableException e) { e.printStackTrace(); }
+		
+		try
+		{
+			String path = "file:/C:/Users/jcrod/Desktop/Programming/Java/MorseCodeProgram/"; // Okay okay I know I shouldn't hard code this but I'm out of time
+			if(shortLength)
+				path += "BeepShort.wav";
+			else
+				path += "BeepLong.wav";
+			URL soundURL = new URL(path);
+			AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
+			clip.open(audioStream);
+		}
+		catch(LineUnavailableException lue) { lue.printStackTrace(); }
+		catch(UnsupportedAudioFileException uafe) { uafe.printStackTrace(); }
+		catch(IOException ioe) { ioe.printStackTrace(); }
+		catch(Exception e)
+		{
+			System.out.println("Error: Beep.wav not found\nExiting Program...");
+			System.exit(0);
+		}
+		
+		clip.start();
+		
+		do
+		{
+			try { Thread.sleep(50); }
+			catch(InterruptedException ie) { ie.printStackTrace(); }
+		} while(clip.isActive());
 	}
 }
