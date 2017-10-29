84630b260fc5e57ae0a1446a12a81a840e99ef4d
Justin Roderman <jcroderman@gmail.com>
2017-10-29 16:56:39 -0500

    Adds files

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Name: Justin Roderman
+ * File: Main.java
+ * Date: October 29, 2017
+ * Desc: Simple program to receive input as string and output as morse code
+ */
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
