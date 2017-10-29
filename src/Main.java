85dc9d1de37637b5e43173f51deb16f6479f26d8
Justin Roderman <jcroderman@gmail.com>
2017-10-29 17:31:37 -0500

    Adds comments because I am dumb and didn't write with comments already

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,22 @@ import java.util.Scanner;
 
 public class Main
 {
-	public static  HashMap<Character, String> morseDictionary;
+	public static  HashMap<Character, String> morseDictionary; // Morse Code Dictionary
 	
 	public static void main(String[] args)
 	{
-		programInitialize();
+		programInitialize(); // Must run first
 		
 		String in = input();
 		System.out.println(convertToMorse(in));
 	}
 	
+	// Populates the morseDictionary
 	public static void programInitialize()
 	{
 		morseDictionary = new HashMap<Character, String>();
 		
+		// Populate the dictionary
 		try
 		{
 			Scanner morseInput = new Scanner(new File("MorseDictionary.txt"));
@@ -35,37 +37,40 @@ public class Main
 				String morse = morseInput.next();
 				morseDictionary.put(key, morse);
 			}
-			morseDictionary.put(' ', " ");
+			morseDictionary.put(' ', "|"); // Spaces are denoted by pipes
 			morseInput.close();
 		}
-		catch (FileNotFoundException e)
+		catch (FileNotFoundException e) // Run if dictionary is not in files
 		{
 			System.out.println("Error: Dictionary not found. Exiting Program...");
 			System.exit(0);
 		}
 	}
 	
+	// Simple user input
 	public static String input()
 	{
+		// Receive input
 		String inputMessage = "";
 		Scanner input = new Scanner(System.in);
 		System.out.print("Please input message: ");
 		inputMessage = input.nextLine();
 		input.close();
 		
-		inputMessage = inputMessage.toLowerCase();
+		inputMessage = inputMessage.toLowerCase(); // Makes all characters lowercase
 		
 		return inputMessage;
 	}
 	
+	// Converts the input to morse code
 	public static String convertToMorse(String input)
 	{
 		String result = "";
 		for(int i = 0; i < input.length(); i++)
 		{
-			String morse = morseDictionary.get(input.charAt(i));
-			if(morse != null)
-				result += (morse + ' ');
+			char key = input.charAt(i);
+			if(morseDictionary.containsKey(key))
+				result += (morseDictionary.get(key) + ' ');
 		}
 		return result;
 	}
